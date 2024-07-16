5ff38a42deb9d7bf184e336e978fac6c0d085aa1
letthem <suyeon3662@naver.com>
2024-07-16 16:49:56 +0900

    Environment: ESLint 충돌 해결 및 폴더 구조 변경

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: { project: ['./tsconfig.json'] },
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'@typescript-eslint/strict-boolean-expressions': [
+			2,
+			{
+				allowString: false,
+				allowNumber: false,
+			},
+		],
+	},
+	ignorePatterns: ['src/**/*.test.ts', 'src/frontend/generated/*'],
+};
