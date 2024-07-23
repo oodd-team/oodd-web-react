ee4129600c94fea919661d3839ee78a6bd25c845
letthem <suyeon3662@naver.com>
2024-07-23 17:39:13 +0900

    Feat: API 설정 추가

---
+++ src/config/constant.ts
@@ -0,0 +1,2 @@
+// JWT를 로컬 스토리지에 저장할 때 사용하는 키. 추후 수정
+export const JWT_KEY = 'jwt_token';
--- src/pages/Chats/index.tsx
+++ src/pages/Chats/index.tsx
@@ -5,6 +5,7 @@ import { OODDFrame } from '../../components/Frame/Frame';
 const Chats: React.FC = () => {
 	return (
 		<OODDFrame>
+			{/* 각자 작업 height에 맞게 조절하여 ChatsContainer을 위치시킵니다. HomeContainer를 참고해주세요. */}
 			채팅페이지입니다
 			<NavBar />
 		</OODDFrame>
