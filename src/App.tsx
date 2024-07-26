b299c7e37b532bffb0eec67d7592b5d121d454f9
young <azerosong@gmail.com>
2024-07-26 12:15:46 +0900

    Feat: Chats & ChatRoom 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Chats from './pages/Chats';
+import ChatRoom from './pages/ChatRoom';
 
 const App: React.FC = () => {
 	return (
@@ -8,6 +10,8 @@ const App: React.FC = () => {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/chats" element={<Chats />}></Route>
+					<Route path="/chats/:id" element={<ChatRoom />}></Route>
 				</Routes>
 			</BrowserRouter>
 		</>
