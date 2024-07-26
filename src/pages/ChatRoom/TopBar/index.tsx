a8791a0672710c715d5cb3ee205be0f4b37f1de6
young <azerosong@gmail.com>
2024-07-26 12:15:10 +0900

    Feat: TopBar 뒤로가기 & 메뉴 기능 추가

--- src/pages/ChatRoom/TopBar/index.tsx
+++ src/pages/ChatRoom/TopBar/index.tsx
@@ -1,14 +1,20 @@
 import { StyledText } from '../../../components/Text/StyledText';
 import theme from '../../../styles/theme';
 import { TopbarLayout, BackButton, KebabMenu, Username } from './styles';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { isClickedMenuAtom } from '../../../recoil/isClickedMenu';
 
 const TopBar: React.FC = () => {
+	const setIsClickedMenu = useSetRecoilState(isClickedMenuAtom);
+	const nav = useNavigate();
 
 	return (
 		<TopbarLayout>
 			<BackButton
 				src="../../../../back.png"
 				alt="back"
+				onClick={()=>{nav(-1);}}
 			/>
 			<Username>
 				<StyledText textTheme={{ style: 'body2-light', lineHeight: 1.5 }} color={theme.colors.black}>
@@ -18,6 +24,9 @@ const TopBar: React.FC = () => {
 			<KebabMenu
 				src="../../../../kebab-menu.png"
 				alt="menu"
+				onClick={() => {
+					setIsClickedMenu(true);
+				}}
 			/>
 		</TopbarLayout>
 	);
