a05e77334d58c297cf371ee70ea24f73c44ae9bf
young <azerosong@gmail.com>
2024-07-26 11:55:34 +0900

    Feat: DateBar(날짜 표시줄) 컴포넌트 UI 구현

---
+++ src/pages/ChatRoom/DateBar/styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const DatebarLayout = styled.div`
+	display: flex;
+	width: 100%;
+	margin: 20px auto 16px auto;
+	gap: 8px;
+`;
+
+export const DateWrapper = styled.div`
+	width: fit-content;
+	white-space: nowrap;
+`;
+
+export const Divider = styled.hr`
+	width: 100%;
+	height: 1px;
+	color: #c4c4c4;
+`;
