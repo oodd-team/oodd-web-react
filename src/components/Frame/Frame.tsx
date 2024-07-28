89949b7fa24ed4cab57e7d7cbc3137dcefa89644
young <azerosong@gmail.com>
2024-07-28 20:25:27 +0900

    Design: OODDFrame px->rem 단위 수정

--- src/components/Frame/Frame.tsx
+++ src/components/Frame/Frame.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 // 공통 레이아웃 -> 모두 적용해주세요
 export const OODDFrame = styled.div`
 	background-color: #ffffff;
-	max-width: 512px;
-	min-height: 844px;
+	max-width: 32rem;
+	min-height: 52.75rem;
 	height: auto;
 	margin: auto;
 	display: flex;
