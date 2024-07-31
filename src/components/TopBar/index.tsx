a80ef090b43b6fff62090ec72d7e15b3634966fe
young <azerosong@gmail.com>
2024-07-31 23:08:35 +0900

    Feat: TopBar 컴포넌트를 feat/OODD-6에서 공통 컴포넌트로 추출

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,28 @@
+import { StyledText } from '../../components/Text/StyledText';
+import theme from '../../styles/theme';
+import { TopbarLayout, BackButton, KebabMenuButton } from './styles';
+import { useNavigate } from 'react-router-dom';
+import Back from '../../../assets/Chats/Back.svg';
+import KebabMenu from '../../../assets/Chats/KebabMenu.svg';
+
+const TopBar: React.FC = () => {
+	const nav = useNavigate();
+
+	return (
+		<TopbarLayout>
+			<BackButton
+				src={Back}
+				alt="back"
+				onClick={() => {
+					nav(-1);
+				}}
+			/>
+			<StyledText $textTheme={{ style: 'body2-light', lineHeight: 1.5 }} color={theme.colors.black}>
+				IDID
+			</StyledText>
+			<KebabMenuButton src={KebabMenu} alt="menu" onClick={() => {}} />
+		</TopbarLayout>
+	);
+};
+
+export default TopBar;
