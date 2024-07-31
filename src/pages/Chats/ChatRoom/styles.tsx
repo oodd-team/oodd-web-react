66bbc54c86289fcdcfd6cd8d88561488e42243e5
young <azerosong@gmail.com>
2024-08-01 00:39:24 +0900

    Feat: ChatRoom 스크롤 기능 추가

--- src/pages/Chats/ChatRoom/styles.tsx
+++ src/pages/Chats/ChatRoom/styles.tsx
@@ -1,12 +1,26 @@
 import styled from 'styled-components';
 
-// TODO: MessagesContainer 내부에 scroll 만드려면 height 설정 필요
+// OODDFrame height: 100vh
+// GlobalStyles body margin-bottom: 0 환경에서 테스트
 export const MessagesContainer = styled.div`
 	width: 100%;
-	overflow: auto;
+	overflow-y: scroll;
 	display: flex;
 	flex: 1;
 	flex-direction: column;
-	padding: 1rem 1.25rem 0 1.25rem;
+	padding: 1rem 0.875rem 0 1.25rem;
 	margin: 2.75rem auto 5.2rem auto;
+	scroll-behavior: smooth;
+
+	&::-webkit-scrollbar {
+		width: 6px;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		background: ${({ theme }) => theme.colors.gray1};
+	}
+
+	&::-webkit-scrollbar-thumb:hover {
+		background: ${({ theme }) => theme.colors.gray2};
+	}
 `;
