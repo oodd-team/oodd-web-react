c4f01e913ecf1bb45089d965984049d0c06010f9
young <azerosong@gmail.com>
2024-08-03 18:49:20 +0900

    Design: ChatList 우측 응답 대기중 색상 변경

--- src/pages/Chats/ChatList/index.tsx
+++ src/pages/Chats/ChatList/index.tsx
@@ -6,9 +6,6 @@ interface ChatListProps {
 	isWaiting: boolean; // 응답 대기중 표시 여부 결정
 }
 
-// TODO: isWaiting 불투명도 영역 설정
-// 와이어프레임에는 ChatList 전체가 불투명도 50%
-// 그 외에는 RrightBox만 불투명도 50%
 const ChatList: React.FC<ChatListProps> = ({ isWaiting }) => {
 	return (
 		<ChatListLayout>
@@ -26,7 +23,7 @@ const ChatList: React.FC<ChatListProps> = ({ isWaiting }) => {
 					30분 전
 				</StyledText>
 				{isWaiting && (
-					<StyledText $textTheme={{ style: 'body6-regular', lineHeight: 1.193 }} color={theme.colors.black}>
+					<StyledText $textTheme={{ style: 'body6-regular', lineHeight: 1.193 }} color={theme.colors.gray3}>
 						응답 대기중
 					</StyledText>
 				)}
