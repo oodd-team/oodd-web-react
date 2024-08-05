2d35c4d70fafb4b0c514de3935a10e9e44583c0b
young <azerosong@gmail.com>
2024-08-05 15:53:59 +0900

    Feat: Comment 컴포넌트 전송 기능 구현

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -3,15 +3,17 @@ import theme from '../../styles/theme';
 import { CommentLayout, SendContainer, CommentTextarea, SendImg } from './styles';
 import Send from '/Send.svg';
 import { useEffect, useRef, useState } from 'react';
+import { CommentProps } from './dto';
 
-const Comment: React.FC = () => {
+const Comment: React.FC<CommentProps> = ({ sendComment }) => {
 	const [comment, setComment] = useState('');
 	const textareaRef = useRef<HTMLTextAreaElement>(null);
 
+	// textarea 내용에 따라 높이 조정
 	useEffect(() => {
 		if (textareaRef.current) {
 			textareaRef.current.style.height = '1.2rem';
-			textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`; // 내용에 맞게 높이를 설정
+			textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
 		}
 	}, [comment]);
 
@@ -23,6 +25,30 @@ const Comment: React.FC = () => {
 		}
 	};
 
+	// textarea에서 enter 입력 시 실행
+	const onKeyDown = (e: any): void => {
+		if (comment === '') {
+			e.preventDefault();
+			return;
+		}
+		if (e.key === 'Enter' && !e.shiftKey) {
+			e.preventDefault();
+			sendComment(e.target.value);
+			setComment('');
+		}
+	};
+
+	// send 이미지 클릭 시 실행
+	const onClickSend = (): void => {
+		if (comment === '') {
+			return;
+		}
+		if (textareaRef.current) {
+			sendComment(textareaRef.current.value);
+		}
+		setComment('');
+	};
+
 	return (
 		<CommentLayout>
 			<StyledText
@@ -30,13 +56,12 @@ const Comment: React.FC = () => {
 				$textTheme={{ style: 'body2-light', lineHeight: 1.5 }}
 				color={theme.colors.gray3}
 			>
-				IDID님에게 대표 이미지와 함께 전달될
-				<br />
-				한줄 메시지를 보내보세요!
+				{/* TODO: 내용 고정? props로 받기? padding에 따라 개행? 고정 개행? 결정 후 수정 */}
+				IDID님에게 대표 이미지와 함께 전달될 한줄 메시지를 보내보세요!
 			</StyledText>
 			<SendContainer>
-				<CommentTextarea ref={textareaRef} value={comment} onChange={onChangeComment} />
-				<SendImg src={Send} />
+				<CommentTextarea ref={textareaRef} value={comment} onChange={onChangeComment} onKeyDown={onKeyDown} />
+				<SendImg src={Send} onClick={onClickSend} />
 			</SendContainer>
 		</CommentLayout>
 	);
