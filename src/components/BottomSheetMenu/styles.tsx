c807e638dad91128f6886dfc16c643b6eacc9522
young <azerosong@gmail.com>
2024-08-05 19:28:49 +0900

    Design: rem 단위 조정으로 일부 값 수정

--- src/components/BottomSheetMenu/styles.tsx
+++ src/components/BottomSheetMenu/styles.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 
 export const BottomSheetMenuLayout = styled.div<{ $marginBottom: string }>`
-	margin: 1.5rem 0 ${(props) => (props ? props.$marginBottom : '0')} 0;
+	margin: 1.8rem 0 ${(props) => (props ? props.$marginBottom : '0')} 0;
 	display: flex;
 	flex-direction: column;
 `;
 
 export const SheetItem = styled.div`
-	padding: 0.8rem 1.2rem;
+	padding: 1rem 1.25rem;
 	width: 100%;
 	display: flex;
 	justify-content: space-between;
@@ -15,6 +15,6 @@ export const SheetItem = styled.div`
 `;
 
 export const Icon = styled.img`
-	width: 1.4rem;
-	height: 1.4rem;
+	width: 1.5rem;
+	height: 1.5rem;
 `;
