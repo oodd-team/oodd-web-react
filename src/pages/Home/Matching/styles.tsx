77acaf586be1c707b34976a1719c62cb4026f7f3
letthem <suyeon3662@naver.com>
2024-08-06 14:05:01 +0900

    Feat: 매칭탭 margin-bottom 설정

--- src/pages/Home/Matching/styles.tsx
+++ src/pages/Home/Matching/styles.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const MatchingContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	margin-bottom: 100px;
 `;
 
 export const Like = styled.div`
