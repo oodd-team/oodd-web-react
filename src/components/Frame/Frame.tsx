1291d8aa6bed9ba90347b5c0d4a3bd5be480cf06
letthem <suyeon3662@naver.com>
2024-08-06 14:00:59 +0900

    Fix: Frame height 수정

--- src/components/Frame/Frame.tsx
+++ src/components/Frame/Frame.tsx
@@ -5,9 +5,8 @@ export const OODDFrame = styled.div`
 	background-color: ${({ theme }) => theme.colors.white};
 	max-width: 512px;
 	min-width: 300px;
-	height: auto;
+	height: 100vh;
 	margin: auto;
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
 `;
