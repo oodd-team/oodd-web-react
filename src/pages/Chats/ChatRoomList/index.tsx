4457bf3cf8ff33c7974681429c42b284c098a8bd
young <azerosong@gmail.com>
2024-08-12 17:55:30 +0900

    Feat: ChatRoomList timeAgo 로직 추가

--- src/pages/Chats/ChatRoomList/index.tsx
+++ src/pages/Chats/ChatRoomList/index.tsx
@@ -6,16 +6,44 @@ import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { OpponentInfoAtom } from '../../../recoil/OpponentInfo';
 import ProfileImg from '../../../../public/ProfileImg.svg';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+import { useEffect, useState } from 'react';
+dayjs.extend(relativeTime);
 
 // createdAt은 어디에 사용?
 const ChatRoomList: React.FC<ChatRoomDto> = ({ id, createdAt, opponent, latestMessage }) => {
 	let isUnread = false;
 	const nav = useNavigate();
 	const [opponentInfo, setOpponentInfo] = useRecoilState(OpponentInfoAtom);
+	const [timeAgo, setTimeAgo] = useState<string | null>(null);
 
-	if (latestMessage.createdAt && latestMessage.toUserReadAt) {
-		isUnread = latestMessage.createdAt.getTime() > latestMessage.toUserReadAt.getTime();
+	if (latestMessage.createdAt) {
+		// toUserReadAt이 내가 마지막으로 읽은 시간이 아니라면
+		// 즉 해당 메시지의 receiver가 해당 메시지를 읽은 시간이라면
+		// toUserReadAt === null일 때 isUnread가 될 수 없고,
+		// (내가 메시지를 보내고 상대방이 읽지 않은 상황에도 응답 대기중 발생)
+		// fromUserId가 필요할 듯
+		isUnread = false;
 	}
+
+	useEffect(() => {
+		if (latestMessage.createdAt) {
+			// 초기 시간 설정
+			setTimeAgo(dayjs(latestMessage.createdAt).fromNow());
+
+			// 60초마다 `timeAgo`를 업데이트
+			const interval = setInterval(() => {
+				setTimeAgo(dayjs(latestMessage.createdAt).fromNow());
+			}, 60000);
+
+			// 컴포넌트 언마운트 시 타이머 정리
+			return () => clearInterval(interval);
+		} else {
+			setTimeAgo(null);
+		}
+	}, [latestMessage.createdAt]);
 
 	const onClickChatRoom = () => {
 		setOpponentInfo(opponent);
@@ -35,9 +63,9 @@ const ChatRoomList: React.FC<ChatRoomDto> = ({ id, createdAt, opponent, latestMe
 			</LeftBox>
 			<RightBox $isUnread={isUnread}>
 				<StyledText $textTheme={{ style: 'body6-regular', lineHeight: 1.193 }} color={theme.colors.gray3}>
-					30분 전
+					{timeAgo}
 				</StyledText>
-				{isUnread && (
+				{timeAgo && isUnread && (
 					<StyledText $textTheme={{ style: 'body6-regular', lineHeight: 1.193 }} color={theme.colors.gray3}>
 						응답 대기중
 					</StyledText>
