8036f6d8c8724f85a7836533032842dd4606a00c
letthem <suyeon3662@naver.com>
2024-08-19 14:59:43 +0900

    Feat: Post 페이지 구현

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { OODDFrame } from '../../components/Frame/Frame';
 import PostTopBar from './PostTopBar';
+import profileImg from './../../assets/Post/profileImg.svg';
+import postImg1 from './../../assets/Post/postImg1.svg';
+import more from './../../assets/Post/more.svg';
+import { StyledText } from '../../components/Text/StyledText';
+import { MoreBtn, PostImg, PostInfo, PostText, UserInfo, UserName, UserProfile } from './styles';
+import theme from '../../styles/theme';
 
 // Post 페이지입니다.
 const Post: React.FC = () => {
 	return (
 		<OODDFrame>
 			<PostTopBar />
-			루룰
+			<PostInfo>
+				<UserInfo>
+					<UserProfile>
+						<img src={profileImg} alt="profileImg" />
+					</UserProfile>
+					<UserName>
+						<StyledText $textTheme={{ style: 'body1-medium', lineHeight: 1 }} color={theme.colors.black}>
+							IDID
+						</StyledText>
+					</UserName>
+				</UserInfo>
+				<MoreBtn>
+					<img src={more} alt="more" />
+				</MoreBtn>
+			</PostInfo>
+			<PostText>
+				<StyledText
+					$textTheme={{ style: 'body6-light', lineHeight: 1.2 }}
+					color={theme.colors.black}
+					style={{ opacity: '50%' }}
+				>
+					Text~~~~~~~~~~~~~~~~~~~~~~~ ...Text~~~~~~~~~~~~~~~~~~~~~~~ ...Text~~~~~~~~~~~~~~~~~~~~~~~ ...
+				</StyledText>
+			</PostText>
+			<PostImg src={postImg1} alt="postImg1" />
 		</OODDFrame>
 	);
 };
--- src/pages/Post/styles.tsx
+++ src/pages/Post/styles.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+// PostTopBar
+
 export const PostTopBarContainer = styled.div`
 	width: 100%;
 	max-width: 32rem;
@@ -39,3 +41,47 @@ export const RightSpace = styled.div`
 	height: 1.125rem;
 	margin-right: 1.3125rem;
 `;
+
+// Post
+
+export const PostInfo = styled.div`
+	display: flex;
+	margin-top: 3.375rem;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+export const UserInfo = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+export const UserProfile = styled.div`
+	width: 2.25rem;
+	height: 2.25rem;
+	overflow: hidden;
+	margin: 0 0.75rem 0 1.25rem;
+	img {
+		width: 100%;
+		height: 100%;
+	}
+`;
+
+export const UserName = styled.div``;
+export const MoreBtn = styled.div`
+	width: 1.5rem;
+	height: 1.5rem;
+	margin-right: 1.25rem;
+`;
+
+export const PostText = styled.span`
+	margin: 0 1.25rem 0.5rem;
+	margin-top: 0.75rem;
+	margin-bottom: 0.4875rem;
+	display: flex;
+	align-items: center;
+	word-wrap: break-word;
+	word-break: break-all;
+`;
+
+export const PostImg = styled.img``;
