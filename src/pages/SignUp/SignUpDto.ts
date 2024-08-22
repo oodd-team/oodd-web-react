8a3fcbb07afe18bc4c4b3a694f97146b0745e1d7
laurel <ajinpark0518@naver.com>
2024-08-22 12:16:06 +0900

    fix: atoms 파일 수정

--- src/pages/SignUp/SignUpDto.ts
+++ src/pages/SignUp/SignUpDto.ts
@@ -1,15 +1,15 @@
 export interface SignUpDto {
-    isSuccess: boolean;
-    code: number;
-    message: string;
-    result: {
-        id: number;
-        name: string;
-        email: string;
-        nickname: string;
-        phoneNumber: string;
-        profilePictureUrl: string;
-        bio: string;
-        joinedAt: string;
-    }
+	isSuccess: boolean;
+	code: number;
+	message: string;
+	result: {
+		id: number;
+		name: string;
+		email: string;
+		nickname: string;
+		phoneNumber: string;
+		profilePictureUrl: string;
+		bio: string;
+		joinedAt: string;
+	};
 }
--- src/recoil/atoms.ts
+++ src/recoil/atoms.ts
@@ -2,31 +2,16 @@ import { atom } from 'recoil';
 import { UserInfoProps } from '../pages/ProfileViewer/dto';
 
 export const userDetailsState = atom<UserInfoProps | null>({
-    key: 'userDetailsState',
-    default: null,
-});
-
-export const userIdState = atom({
-    key: 'userIdState',
-    default: null,
-});
-
-export const tokenState = atom({
-    key: 'tokenState',
-    default: null,
+	key: 'userDetailsState',
+	default: null,
 });
 
 export const isBottomSheetOpenState = atom<boolean>({
-    key: 'isBottomSheetOpenState',
-    default: false,
+	key: 'isBottomSheetOpenState',
+	default: false,
 });
 
 export const friendState = atom<boolean>({
-    key: 'friendState',
-    default: false,
-});
-
-export const authTokenState = atom({
-    key: 'authTokenState', // 유니크한 key를 지정합니다.
-    default: '', // 기본값을 빈 문자열로 설정합니다.
+	key: 'friendState',
+	default: false,
 });
