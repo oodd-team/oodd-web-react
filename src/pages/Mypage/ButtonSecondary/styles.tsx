06f8f1955a30f8ab0f8d71d270e4667156138bf0
laurel <ajinpark0518@naver.com>
2024-08-23 05:00:20 +0900

    fix: 마이페이지 상단 바 고정

--- src/pages/Mypage/ButtonSecondary/styles.tsx
+++ src/pages/Mypage/ButtonSecondary/styles.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
 	width: calc(100% - 60px); /* 양옆에 30px씩 공간을 확보 */
-	padding: 10px;
+	padding: 6px;
 	margin: 1.25rem auto;
 
 	border: 1px solid #000;
-	border-radius: 0.3125rem;
+	border-radius: 10px;
 	height: 3.1rem; /* 44px */
 	text-align: center;
 	cursor: pointer;
