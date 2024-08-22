8713421f694f024cb1cc0110cf885cc3179b8267
laurel <ajinpark0518@naver.com>
2024-08-22 17:00:07 +0900

    Fix: cicd 충돌 해결

--- src/pages/Chats/RecentChat/styles 2.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const RecentChatInfo = styled.div`
-	margin: 0.875rem auto 0.688rem auto;
-	padding-left: 1.25rem;
-	width: 100%;
-`;
--- src/pages/Chats/Request/styles 2.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const ReqeustInfo = styled.div`
-	margin: 1.31rem 1.25rem 0.145rem 1.25rem;
-	text-align: left;
-`;
