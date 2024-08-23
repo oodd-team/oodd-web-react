c684fd04fe78302ded403ad2226d42dc6e1f692b
지민재 <jim400934@gmail.com>
2024-08-23 22:10:50 +0900

    24.08.23 merge fixing

--- src/pages/Mypage/index.tsx
+++ src/pages/Mypage/index.tsx
@@ -13,45 +13,100 @@ import {
 	StatNumber,
 	StatLabel,
 	PostsContainer,
+	AddButton,
 } from './styles';
 import { OODDFrame } from '../../components/Frame/Frame';
 import NavbarProfile from '../../components/NavbarProfile';
 import NavBar from '../../components/NavBar';
-import avatarImage from '../../assets/avatar.png';
 import ButtonSecondary from './ButtonSecondary';
 import Post from './Post';
-import request from '../../apis/core';
-import ProfileActions from '../Profile';
+import request, { BaseResponse } from '../../apis/core';
 import { PostItem, PostsResponse, UserResponse } from './dto';
+import imageBasic from '../../assets/imageBasic.svg';
+import Loading from '../../components/Loading';
+import BottomSheet from '../../components/BottomSheet';
+import { BottomSheetProps } from '../../components/BottomSheet/dto';
+import BottomSheetMenu from '../../components/BottomSheetMenu';
+import { BottomSheetMenuProps } from '../../components/BottomSheetMenu/dto';
+import button_plus from '../../assets/Profile/button_plus.svg';
+import Insta from '../../assets/BottomSheetMenu/Insta.svg';
+import Picture from '../../assets/BottomSheetMenu/Picture.svg';
 
-const Mypage: React.FC = () => {
-	const navigate = useNavigate();
+const MyPage: React.FC = () => {
 	const [user, setUser] = useState<UserResponse | null>(null);
 	const [posts, setPosts] = useState<PostItem[]>([]);
 	const [totalPosts, setTotalPosts] = useState(0);
 	const [totalLikes, setTotalLikes] = useState(0);
 	const [totalComments, setTotalComments] = useState(0); // Comments count
-	
+	const [isLoading, setIsLoading] = useState(true); // 로딩 상태 추가
+	const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+	const navigate = useNavigate();
+
+	const bottomSheetMenuProps: BottomSheetMenuProps = {
+		items: [
+			{
+				text: '인스타 피드 가져오기',
+				action: () => {
+					setIsBottomSheetOpen(false);
+					handleInstagramSelect();
+				},
+				icon: Insta,
+			},
+			{
+				text: '사진 올리기',
+				action: () => {
+					setIsBottomSheetOpen(false);
+					handlePhotoUploadSelect();
+				},
+				icon: Picture,
+			},
+		],
+		marginBottom: '50px',
+	};
+
+	const bottomSheetProps: BottomSheetProps = {
+		isOpenBottomSheet: isBottomSheetOpen,
+		isHandlerVisible: true,
+		Component: BottomSheetMenu,
+		componentProps: bottomSheetMenuProps,
+		onCloseBottomSheet: () => {
+			setIsBottomSheetOpen(false);
+		},
+	};
+
+	const handleOpenSheet = () => {
+		setIsBottomSheetOpen(true);
+	};
+
+	const handleInstagramSelect = () => {
+		navigate('/upload', { state: { mode: 'instagram' } });
+	};
+
+	const handlePhotoUploadSelect = () => {
+		navigate('/upload', { state: { mode: 'image' } });
+	};
+
 	const handlePostClick = (postId: string) => {
 		navigate(`/my-post/${postId}`);
 	};
+
 	// 사용자 정보 가져오기 함수
 	const fetchUserData = async () => {
 		try {
-			// userId를 localStorage에서 가져옴
-			const storedUserId = localStorage.getItem('id'); // 또는 상태관리에서 가져오기
+			const storedUserId = localStorage.getItem('id');
 
 			if (!storedUserId) {
 				console.error('User is not logged in');
 				return;
 			}
 
-			const response = await request.get<UserResponse>(`/users/${storedUserId}`);
-			setUser(response);
+			const response = await request.get<BaseResponse<UserResponse>>(`/users/${storedUserId}`);
+			setUser(response.result);
 		} catch (error) {
 			console.error('Error fetching user data:', error);
 		}
 	};
+
 	// API에서 포스트 리스트를 가져오는 함수
 	const handlePostList = async () => {
 		try {
@@ -67,7 +122,6 @@ const Mypage: React.FC = () => {
 				setTotalPosts(totalPosts);
 				setTotalLikes(totalLikes);
 				setPosts(posts);
-				// 코멘트 수 합산
 				const totalComments = posts.reduce((sum, post) => sum + (post.commentsCount || 0), 0);
 				setTotalComments(totalComments);
 			} else {
@@ -75,21 +129,31 @@ const Mypage: React.FC = () => {
 			}
 		} catch (error) {
 			console.error('Error fetching posts:', error);
+		} finally {
+			setIsLoading(false); // 로딩 완료 후 로딩 상태 false로 설정
 		}
 	};
-	// 컴포넌트가 마운트될 때 사용자 정보와 포스트 리스트를 가져옴
+
 	useEffect(() => {
 		fetchUserData();
 		handlePostList();
 	}, []);
 
+	if (isLoading) {
+		return <Loading />; // 로딩 중일 때 Loading 컴포넌트 표시
+	}
+
 	return (
 		<OODDFrame>
 			<ProfileContainer>
+				<AddButton onClick={handleOpenSheet}>
+					<img src={button_plus} />
+				</AddButton>
+				<BottomSheet {...bottomSheetProps} />
 				<NavbarProfile />
 				<Header>
 					<AvatarWrapper>
-						<Avatar src={user?.profilePictureUrl || avatarImage} alt="User Avatar" />
+						<Avatar src={user?.profilePictureUrl || imageBasic} alt="User Avatar" />
 					</AvatarWrapper>
 					<UserInfo>
 						<Username>{user?.name || '김아무개...'}</Username>
@@ -114,9 +178,9 @@ const Mypage: React.FC = () => {
 				<PostsContainer>
 					{posts
 						.sort((a, b) => {
-							if (b.isRepresentative && !a.isRepresentative) return 1; // b가 대표 포스트일 때 a보다 앞에 위치
-							if (a.isRepresentative && !b.isRepresentative) return -1; // a가 대표 포스트일 때 b보다 앞에 위치
-							return 0; // 둘 다 대표 포스트가 아니거나 둘 다 대표 포스트일 경우, 순서를 유지
+							if (b.isRepresentative && !a.isRepresentative) return 1;
+							if (a.isRepresentative && !b.isRepresentative) return -1;
+							return 0;
 						})
 						.map((post) => (
 							<Post
@@ -129,10 +193,10 @@ const Mypage: React.FC = () => {
 							/>
 						))}
 				</PostsContainer>
-				<ProfileActions /> {/* ProfileActions 컴포넌트 추가 */}
 				<NavBar />
 			</ProfileContainer>
 		</OODDFrame>
 	);
 };
-export default Mypage;
+
+export default MyPage;
