60ac53dbff74dd43e556655a30bc6b92dc85232a
lalaurel <ajinpark0518@naver.com>
2024-10-08 03:01:16 +0900

    "Refactor: new 폰트 및 사이즈 적용한 컴포넌트 생성"

--- src/components/Text/StyledText.tsx
+++ src/components/Text/StyledText.tsx
@@ -9,6 +9,7 @@ export interface StyledTextProps {
 	color?: string;
 }
 
+//디자인 ver1.0 폰트 전 페이지 적용 후에 삭제 에정
 export const StyledText = styled.div<StyledTextProps>`
 	${(props) => props.theme.fontStyles[props.$textTheme.style]};
 	line-height: ${(props) => props.$textTheme.lineHeight}rem;
@@ -16,24 +17,86 @@ export const StyledText = styled.div<StyledTextProps>`
 	white-space: pre-line;
 `;
 
-//StyledText 컴포넌트
-const Body1MediumText = styled.div`
-  font-weight: 500; /* or the weight of 'body1-medium' */
-  line-height: 2;
-  color: ${({ theme }) => theme.colors.black};
+//디자인 ver1.0 폰트입니다 적용해주세요
+export const Body2 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 15px;                        
+  font-weight: 700;                       
+  line-height: 22.01px;                   
+  color: ${({ theme }) => theme.colors.black};  
 `;
 
-const Body2LightText = styled.div`
-  font-weight: 300; /* or the weight of 'body2-light' */
-  line-height: 1;
-  color: ${({ theme }) => theme.colors.black};
+export const Headline2 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 17px;                        
+  font-weight: 500;                       
+  line-height: 24px;                   
+  color: rgba(29, 29, 29, 1)
 `;
 
-const Body4LightGrayText = styled.span`
-  font-weight: 300; /* or the weight of 'body4-light' */
-  line-height: 1;
-  color: ${({ theme }) => theme.colors.gray3};
+export const Display2 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 40px;                        
+  font-weight: 500;                       
+  line-height: 52px;                   
+  color: rgba(0, 0, 0, 1);
 `;
 
-export { Body1MediumText, Body2LightText, Body4LightGrayText };
+export const HakgyoansimPuzzle = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 24px;                        
+  font-weight: 900;                       
+  line-height: 24px;                   
+  color: rgba(255, 35, 137, 1);
+`;
+
+export const Cation2 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 11px;                        
+  font-weight: 400;                       
+  line-height: 14px;                   
+  color: rgba(29, 29, 29, 1);
+`;
+
+export const Body1 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 16px;                        
+  font-weight: 500;                       
+  line-height: 24px;                   
+  color: rgba(123, 123, 123, 1));
+`;
+
+export const Headline1 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 18px;                        
+  font-weight: 700;                       
+  line-height: 26.01px;                   
+  color: rgba(29, 29, 29, 1);
+`;
+
+export const Cation1 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 12px;                        
+  font-weight: 400;                       
+  line-height: 16.01px;                   
+  color: rgba(0, 0, 0, 1);
+`;
+
+export const Title1 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 36px;                        
+  font-weight: 700;                       
+  line-height: 48.02px;                   
+  color: rgba(29, 29, 29, 1);
+`;
+
+export const Heading1 = styled.div`
+  font-family: 'Pretendard', sans-serif;  
+  font-size: 22px;                        
+  font-weight: 400;                       
+  line-height: 30.01px;                   
+  color: rgba(29, 29, 29, 1);
+`;
+
+
 
