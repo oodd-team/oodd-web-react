c10f1b8bdf9e56b7a24b1c3716207700fbb73084
지민재 <jim400934@gmail.com>
2024-10-07 20:47:55 +0900

    Fix: 플랫폼별 margin-top 길이 조정

--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -4,7 +4,6 @@ export const LoginContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
 	width: 100%;
 	max-width: 32rem; /* 최대 너비 512px */
 	height: 100%;
@@ -20,6 +19,16 @@ export const WelcomeWrapper = styled.section`
 	height: 5rem;
 	text-align: center;
 	margin-bottom: 1.5rem;
+	margin-top: 14rem;
+
+	/* 375px ~ 767px 사이에서는 점차적으로 커짐 */
+	@media (min-width: 23.5rem) and (max-width: 47.875rem) {
+		margin-top: 75%;
+	}
+	/* 768px 이상에서는 margin-bottom이 27rem로 고정 */
+	@media (min-width: 47.9375rem) {
+		margin-top: 27rem;
+	}
 `;
 
 export const Service = styled.button`
