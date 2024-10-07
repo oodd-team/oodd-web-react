702b9271148d7ed8aa84e142d994fa698430e91c
지민재 <jim400934@gmail.com>
2024-10-07 20:25:09 +0900

    Fix: 플랫폼별 버튼 너비 조절

--- src/pages/Login/components/style.tsx
+++ src/pages/Login/components/style.tsx
@@ -3,9 +3,17 @@ import styled from 'styled-components';
 export const SocialLogin = styled.button<{ $bgColor: string; $border?: boolean }>`
 	display: flex;
 	align-items: center;
-	width: 100%;
-	max-width: 20rem;
-	height: 3.5rem; /* 56px / 16 */
+	width: 20rem;
+	/* 375px ~ 767px 사이에서는 점차적으로 커짐 */
+	@media (min-width: 23.5rem) and (max-width: 47.875rem) {
+		width: calc(100vw - 3.5rem);
+	}
+
+	/* 768px 이상에서는 버튼 너비가 40rem로 고정 */
+	@media (min-width: 47.9375rem) {
+		width: 40rem;
+	}
+	height: 3.5rem;
 	background-color: ${({ $bgColor }) => $bgColor};
 	border-radius: 0.5rem;
 	border: ${({ $border }) => ($border ? '1px solid #000' : 'none')};
--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -4,9 +4,10 @@ export const LoginContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	justify-content: center;
 	width: 100%;
 	max-width: 32rem; /* 최대 너비 512px */
-	height: auto;
+	height: 100%;
 	margin: 0 auto; /* 중앙 정렬 */
 	//box-shadow: 0 0 0.625rem rgba(0, 0, 0, 0.1); /* 경계 구분용*/
 `;
@@ -18,7 +19,7 @@ export const WelcomeWrapper = styled.section`
 	width: 100%;
 	height: 5rem;
 	text-align: center;
-	margin: 14rem 0 1.5rem 0;
+	margin-bottom: 1.5rem;
 `;
 
 export const Service = styled.button`
@@ -27,5 +28,5 @@ export const Service = styled.button`
 	width: 100%;
 	justify-content: center;
 	padding: 20px;
-	margin: 6.875rem 0 12.625rem 0;
+	margin-top: 6.875rem;
 `;
