eb1bbf6ef7e74602d58ddf93fa80e98e9990a888
지민재 <jim400934@gmail.com>
2024-10-08 16:25:42 +0900

    Fix: margin  미디어 쿼리 말고 그냥 중앙정렬 시킴

--- src/pages/Login/styles.tsx
+++ src/pages/Login/styles.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const LoginContainer = styled.div`
 	display: flex;
 	flex-direction: column;
+	justify-content: center;
 	align-items: center;
 	width: 100%;
 	max-width: 32rem; /* 최대 너비 512px */
@@ -19,16 +20,17 @@ export const WelcomeWrapper = styled.section`
 	height: 5rem;
 	text-align: center;
 	margin-bottom: 1.5rem;
-	margin-top: 14rem;
+	//margin-top: 14rem;
 
-	/* 375px ~ 767px 사이에서는 점차적으로 커짐 */
+	/* 375px ~ 767px 사이에서는 점차적으로 커짐
 	@media (min-width: 23.5rem) and (max-width: 47.875rem) {
 		margin-top: 75%;
 	}
-	/* 768px 이상에서는 margin-top이 27rem로 고정 */
+	 768px 이상에서는 margin-top이 27rem로 고정 
 	@media (min-width: 47.9375rem) {
 		margin-top: 27rem;
 	}
+	*/
 `;
 
 export const Service = styled.button`
