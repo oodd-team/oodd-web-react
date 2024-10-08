e11ee56dc98ded9be3e0aef0a0a47e262dfda2e6
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-10-08 12:59:15 +0900

    Feat: PostComment api 응답에 대한 공통 DTO 정의

---
+++ src/apis/dtos/PostCommentDto.ts
@@ -0,0 +1,48 @@
+import { BaseApiResponse } from './util/ApiResponse';
+
+// 댓글 작성 API 응답
+export type CreateCommentApiResponse = BaseApiResponse<CreateCommentResponse>;
+
+// 댓글 삭제 API 응답
+export type DeleteCommentApiResponse = BaseApiResponse<DeleteCommentResponse>;
+
+// 댓글 조회 API 응답
+export type CommentsApiResponse = BaseApiResponse<CommentsResponse>;
+
+// 공통 필드 인터페이스
+interface BaseComment {
+	id: number;
+	userId: number;
+	postId: number;
+	content: string;
+	createdAt: string;
+	updatedAt: string;
+}
+
+// 댓글 관련 User 정보
+export interface CommentUser {
+	id: number;
+	nickname: string;
+	profilePictureUrl: string;
+}
+
+// 댓글 인터페이스
+export interface Comment extends BaseComment {
+	status: 'activated' | 'deactivated';
+	deletedAt?: string | null;
+	user?: CommentUser;
+}
+
+// 댓글 작성 응답
+export interface CreateCommentResponse extends BaseComment {}
+
+// 댓글 삭제 응답
+export interface DeleteCommentResponse extends BaseComment {
+	status: 'deactivated';
+	deletedAt: string;
+}
+
+// 댓글 조회 응답
+export interface CommentsResponse {
+	comments: Comment[];
+}
