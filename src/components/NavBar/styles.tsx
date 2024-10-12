6364f8c53c51a16cb51fa45bc05093fb6612df3f
young <azerosong@gmail.com>
2024-10-12 17:11:06 +0900

    Fix: fixed 컴포넌트 NavBar도 breakPoint에 따라 동작하도록 수정

--- src/components/NavBar/styles.tsx
+++ src/components/NavBar/styles.tsx
@@ -1,15 +1,20 @@
 import styled from 'styled-components';
+import theme from '../../styles/theme';
 
 export const NavBarContainer = styled.nav`
+	// fixed 포지션에 breakPoint를 적용하는 방법
 	position: fixed;
-	width: 100%;
-	max-width: 32rem;
+	${theme.breakPoints}; // breakPoint 미디어쿼리
+	bottom: 0; // 경우에 따라 top 0 등으로 작성
+	left: 50%; // 수직 중앙에 위치
+	transform: translateX(-50%); // width에 따른 수직 중앙 조정
+	width: 100%; // brakePoint에 따른 width에 따르도록 설정
+
+	// 이후로 자기 코드에 맞춰서 작성
 	height: 5.5rem;
 	justify-content: center;
 	align-items: center;
-	bottom: 0;
 	background-color: white;
-	max-width: 32rem;
 	margin: 0;
 	border-radius: 1.25rem 1.25rem 0 0;
 	filter: drop-shadow(0rem 0rem 0.25rem rgba(0, 0, 0, 0.25));
