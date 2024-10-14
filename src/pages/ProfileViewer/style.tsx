5b346f11798f42fcccbabdf3f28fa8c541f145e3
지민재 <jim400934@gmail.com>
2024-10-14 13:12:42 +0900

    Coment: 좌우 패딩 반응형 부분 주석 처리

--- src/pages/ProfileViewer/style.tsx
+++ src/pages/ProfileViewer/style.tsx
@@ -7,17 +7,17 @@ export const ProfileViewerContainer = styled.main`
 	position: relative;
 	padding: 0 20px; /* 기본 패딩값 */
 
-	@media (min-width: 375px) {
-		padding: 0 calc(20px + (44 * ((100vw - 375px) / 393))); /* 375px부터 768px 사이에서 서서히 증가 */
-	}
+	// @media (min-width: 375px) {
+	// 	padding: 0 calc(20px + (44 * ((100vw - 375px) / 393))); /* 375px부터 768px 사이에서 서서히 증가 */
+	// }
 
-	@media (min-width: 768px) and (max-width: 1920px) {
-		padding: 0 calc(64px + (356 * ((100vw - 768px) / 1152))); /* 768px부터 1920px 사이에서 서서히 증가 */
-	}
+	// @media (min-width: 768px) and (max-width: 1920px) {
+	// 	padding: 0 calc(64px + (356 * ((100vw - 768px) / 1152))); /* 768px부터 1920px 사이에서 서서히 증가 */
+	// }
 
-	@media (min-width: 1920px) {
-		padding: 0 420px; /* 1920px 이상에서는 420px로 고정 */
-	}
+	// @media (min-width: 1920px) {
+	// 	padding: 0 420px; /* 1920px 이상에서는 420px로 고정 */
+	// }
 `;
 
 export const Vector = styled.div`
