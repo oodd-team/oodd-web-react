21604046f3f221b22d3ee96c2cfa3c58584ca6a2
lalaurel <ajinpark0518@naver.com>
2024-10-15 13:01:49 +0900

    feat: 버튼 색상 수정

--- src/pages/Mypage/ButtonSecondary/styles.tsx
+++ src/pages/Mypage/ButtonSecondary/styles.tsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
+import theme from '../../../styles/theme';
 
 export const Button = styled.button`
 	width: calc(100% - 30px); /* 양옆에 30px씩 공간을 확보 */
 	padding: 6px;
 	margin: 1.25rem auto;
-
-	border: 1px solid #000;
-	border-radius: 10px;
 	height: 3.1rem; /* 44px */
 	text-align: center;
+	color: #FF2389;
 	cursor: pointer;
 	box-sizing: border-box;
+  border: 1px solid;  
+	border-radius: 10px;
+  border-color: #FF2389;
+  padding: 10px;  /* 텍스트가 보더와 겹치지 않게 패딩 설정 */
+	
 `;
