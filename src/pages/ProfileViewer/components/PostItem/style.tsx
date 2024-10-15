4597a58e58709f14adfe002c63e903f2a9a7332a
지민재 <jim400934@gmail.com>
2024-10-15 17:41:40 +0900

    Refactor: 게시물 정렬 수정

--- src/pages/ProfileViewer/components/PostItem/style.tsx
+++ src/pages/ProfileViewer/components/PostItem/style.tsx
@@ -10,12 +10,20 @@ export const PostItemContainer = styled.article`
 	flex-direction: column;
 	position: relative; /* LikesOverlay 위치 조정을 위한 설정 */
 	box-sizing: border-box;
-	@media (max-width: 33.125rem) {
-		flex: 1 1 calc(50% - 0.5rem); /* 작은 화면에서도 두 개씩 배치되도록 설정 */
-		max-width: 50%; /* 화면 크기에 따라 너비를 조정하여 두 개씩 배치 */
-		height: auto; /* 높이 자동 조정 */
-		aspect-ratio: 1 / 1; /* 작은 화면에서도 정사각형 유지 */
-	}
+
+	// @media (min-width: 33.125rem) {
+	// 	&:nth-child(2n + 1):last-child {
+	// 		flex: 0 1 calc(50% - 0.5rem); /* 마지막 게시물이 남을 때 왼쪽에 위치 */
+	// 		margin-right: auto; /* 오른쪽 여백을 자동으로 설정하여 왼쪽 정렬 */
+	// 	}
+	// }
+
+	// @media (max-width: 33.125rem) {
+	// 	flex: 1 1 calc(50% - 0.5rem); /* 작은 화면에서도 두 개씩 배치되도록 설정 */
+	// 	max-width: 50%; /* 화면 크기에 따라 너비를 조정하여 두 개씩 배치 */
+	// 	height: auto; /* 높이 자동 조정 */
+	// 	aspect-ratio: 1 / 1; /* 작은 화면에서도 정사각형 유지 */
+	// }
 `;
 
 export const PostImageContainer = styled.figure`
