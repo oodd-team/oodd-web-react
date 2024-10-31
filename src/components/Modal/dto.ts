5cb921d8467438cbfb3c1b2f3acc703d11603ce4
young <azerosong@gmail.com>
2024-10-31 14:01:34 +0900

    Feat: Modal 컴포넌트 UI 수정

--- src/components/Modal/dto.ts
+++ src/components/Modal/dto.ts
@@ -1,4 +1,9 @@
 export interface ModalProps {
+	isCloseButtonVisible?: boolean; // 우측 상단 X 버튼 유무 (default false)
 	onClose: () => void;
 	content: string;
+	button?: {
+		content: string; // 버튼 내부 텍스트 (default 확인)
+		onClick: () => void; // 버튼 클릭 이벤트 핸들러 (default onClose)
+	};
 }
--- src/components/Modal/styles.tsx
+++ src/components/Modal/styles.tsx
@@ -13,19 +13,44 @@ export const ModalWrapper = styled.div`
 	justify-content: center;
 `;
 
-export const ModalContainer = styled.div`
+export const ModalContainer = styled.div<{ $isCloseButtonVisible: boolean }>`
+	position: relative;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
 	text-align: center;
+	gap: 1rem;
 	width: 21.25rem;
 	max-width: calc(100% - 2.5rem);
-	height: 10rem;
 	max-height: 30%;
-	padding: 10px;
+	padding: 1.25rem;
+	${({ $isCloseButtonVisible }) => ($isCloseButtonVisible ? 'padding-top: 2.5rem' : '')};
 	background-color: ${({ theme }) => theme.colors.white};
 	border-radius: 0.625rem;
 	box-shadow: 0 -0.125rem 0.625rem rgba(0, 0, 0, 0.1);
 	z-index: 999;
+`;
+
+export const CloseButton = styled.button`
+	width: 1.88rem;
+	height: 1.88rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: absolute;
+	top: 1.25rem;
+	right: 1.25rem;
+`;
+
+export const ConfirmButton = styled.button`
+	display: flex;
+	width: 7.125rem;
+	padding: 0.625rem 0.875rem;
+	justify-content: center;
+	align-items: center;
+	background: ${({ theme }) => theme.colors.gradient};
+	border-radius: 0.5rem;
+	color: white;
+	${({ theme }) => theme.fontStyles['body1-medium']}
 `;
