8ffd5e38746afb29ae57d33a556161a25f5bb813
young <azerosong@gmail.com>
2024-11-08 02:27:55 +0900

    Comment: api 모듈 소개 주석 추가

--- src/apis/Matching/index.ts
+++ src/apis/Matching/index.ts
@@ -1,5 +1,6 @@
 import { newRequest } from '../core';
 import { CreateMatchingRequest, CreateMatchingResponse } from './dto';
 
+// 매칭 생성
 export const createMatchingApi = (data: CreateMatchingRequest) =>
 	newRequest.post<CreateMatchingResponse>('/matching', data);
