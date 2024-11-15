706392c33ab45456f8d4e22c6c736e3624a0ea7a
young <azerosong@gmail.com>
2024-11-15 10:18:59 +0900

    Design: BottomSheetMenu UI 조정

--- src/components/BottomSheetMenu/index.tsx
+++ src/components/BottomSheetMenu/index.tsx
@@ -1,5 +1,4 @@
 import { StyledText } from '../Text/StyledText';
-import theme from '../../styles/theme';
 import { BottomSheetMenuLayout, SheetItem, Icon } from './styles';
 import { BottomSheetMenuProps, SheetItemDto } from './dto';
 import React from 'react';
@@ -10,7 +9,7 @@ const BottomSheetMenu: React.FC<BottomSheetMenuProps> = React.memo(({ items }) =
 			{items.map((item: SheetItemDto, index) => (
 				<div key={index}>
 					<SheetItem onClick={item.action}>
-						<StyledText $textTheme={{ style: 'body2-regular', lineHeight: 1.5 }} color={theme.colors.black}>
+						<StyledText $textTheme={{ style: 'body1-medium' }} color={'#7b7b7b'}>
 							{item.text}
 						</StyledText>
 						{item.icon && <Icon src={item.icon} />}
