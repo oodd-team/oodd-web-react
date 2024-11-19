c085b945c456854a1566e408b005dd4e385c50e0
지민재 <jim400934@gmail.com>
2024-11-19 17:19:49 +0900

    Refactor: api 함수명 변경

--- src/apis/user/dto.ts
+++ src/apis/user/dto.ts
@@ -13,43 +13,28 @@ export interface UserInfoData {
 	isFriend?: boolean;
 }
 
-export type PostUserReportRequest = ReportRequest;
+// 사용자 정보 조회 응답
+export type GetUserInfoResponse = BaseSuccessResponse<UserInfoData>;
 
-// 사용자 신고 요청
-export interface ReportRequest {
+// 사용자 신고 요청 데이터
+export interface PostUserReportRequest {
 	fromUserId: number;
 	toUserId: number;
 	reason: string;
 }
 
-// 사용자 신고 응답
-export interface ReportUserResult {
-	message: string;
-}
-
-export type PostUserBlockRequest = BlockRequest;
-
 // 차단/해제 요청 데이터
-export interface BlockRequest {
+export interface PostUserBlockRequest {
 	fromUserId: number;
 	toUserId: number;
 	action: 'block' | 'unblock'; // 차단 또는 해제
 }
 
-// 차단/해제 결과 응답
-export interface BlockResult {
-	message: string;
-}
-
-// 사용자 정보 조회 응답
-export type GetUserInfoResponse = BaseSuccessResponse<UserInfoData>;
-
-// 사용자 정보 수정 요청 및 응답
-export type PatchUserUpdateRequest = UpdateUserRequest;
-export type PatchUserUpdateResponse = BaseSuccessResponse<UserInfoData>;
+// 사용자 정보 수정 응답
+export type PatchUserInfoResponse = BaseSuccessResponse<UserInfoData>;
 
 // 사용자 정보 수정 요청 데이터
-export interface UpdateUserRequest {
+export interface PatchUserInfoRequest {
 	name: string;
 	phoneNumber: string;
 	email: string;
@@ -58,11 +43,11 @@ export interface UpdateUserRequest {
 	bio: string;
 }
 
-// 회원 탈퇴
-export type PatchUserDeleteResponse = BaseSuccessResponse<DeleteUserResult>;
+// 회원 탈퇴 응답
+export type PatchUserWithDrawResponse = BaseSuccessResponse<PatchUserWithdrawData>;
 
 // 회원 탈퇴 응답 데이터
-export interface DeleteUserResult {
+export interface PatchUserWithdrawData {
 	isSuccess: boolean;
 	code: string;
 	data: Record<string, never>; // 탈퇴 성공 시 항상 빈 객체가 응답으로 온다면 Record<string, never>으로 타입 안정성 높일 수 있답니다
--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -1,20 +1,20 @@
 import {
 	PostUserBlockRequest,
 	PostUserReportRequest,
-	PatchUserUpdateRequest,
-	PatchUserUpdateResponse,
-	PatchUserDeleteResponse,
+	PatchUserInfoRequest,
+	PatchUserInfoResponse,
+	PatchUserWithDrawResponse,
 } from './dto';
 import { newRequest } from '../core';
 import { EmptySuccessResponse } from '../core/dto';
 
 // 유저 정보 수정 api
-export const patchUserUpdateApi = (data: PatchUserUpdateRequest, userId: string) =>
-	newRequest.patch<PatchUserUpdateResponse>(`/users/${userId}`, data);
+export const patchUserInfoApi = (data: PatchUserInfoRequest, userId: string) =>
+	newRequest.patch<PatchUserInfoResponse>(`/users/${userId}`, data);
 
 // 유저 탈퇴 api
 export const patchUserWithdrawApi = (userId: string) =>
-	newRequest.patch<PatchUserDeleteResponse>(`/user/${userId}/withdraw`);
+	newRequest.patch<PatchUserWithDrawResponse>(`/user/${userId}/withdraw`);
 
 // 유저 차단 api
 export const postUserBlockApi = (data: PostUserBlockRequest) =>
@@ -25,4 +25,4 @@ export const postUserReportApi = (data: PostUserReportRequest) =>
 	newRequest.post<EmptySuccessResponse>('/user-report', data);
 
 // 이용 약관 동의 api
-export const postAgreementUserApi = (userId: string) => newRequest.post<EmptySuccessResponse>(`/user/${userId}`);
+export const postTermsAgreementApi = (userId: string) => newRequest.post<EmptySuccessResponse>(`/user/${userId}`);
