000d6f3033692b4b21efff9c0c49bbdcaf78849d
지민재 <jim400934@gmail.com>
2024-11-19 09:51:10 +0900

    Refact: 유저 차단, 신고 api 모듈화

--- src/apis/User/dto.ts
+++ src/apis/User/dto.ts
@@ -1,40 +1,6 @@
 import { BaseSuccessResponse } from '../core/dto';
 import { BaseApiResponse } from '../util/dto';
 
-// 회원 탈퇴
-export type DeleteUserResponse = BaseApiResponse<DeleteUserResult>;
-
-// 사용자 신고 요청 맟 응답
-export type UpdateUserReportRequest = ReportRequest;
-export type UpdateUserReportResponse = BaseSuccessResponse<ReportUserResult>;
-
-// 사용자 정보 조회
-export type GetUserResponse = BaseApiResponse<User>;
-
-// 사용자 정보 수정
-export type UpdateUserResponse = BaseSuccessResponse<User>;
-
-// 사용자 차단/해제 요청 및 응답
-export type CreateUserBlockRequest = BlockRequest;
-export type CreateUserBlockResponse = BaseSuccessResponse<BlockResult>;
-
-// 회원 탈퇴 응답 데이터
-export interface DeleteUserResult {
-	message: string;
-}
-
-// 사용자 신고 요청
-export interface ReportRequest {
-	fromUserId: number;
-	toUserId: number;
-	reason: string;
-}
-
-// 사용자 신고 응답
-export interface ReportUserResult {
-	message: string;
-}
-
 // 사용자 정보 공통 인터페이스
 export interface User {
 	id: number;
@@ -48,14 +14,48 @@ export interface User {
 	isFriend?: boolean;
 }
 
+// 사용자 신고 요청 맟 응답 -> 스웨거 보니 응답 없는데 삭제 필요할 듯
+export type UpdateUserReportRequest = ReportRequest;
+export type UpdateUserReportResponse = BaseSuccessResponse<ReportUserResult>;
+
+// 사용자 신고 요청
+export interface ReportRequest {
+	fromUserId: number;
+	toUserId: number;
+	reason: string;
+}
+
+// 사용자 신고 응답
+export interface ReportUserResult {
+	message: string;
+}
+
+// 사용자 차단/해제 요청 및 응답 -> 스웨거 보니 응답 없는데 삭제 필요할 듯
+export type CreateUserBlockRequest = BlockRequest;
+export type CreateUserBlockResponse = BaseSuccessResponse<BlockResult>;
+
 // 차단/해제 요청 데이터
 export interface BlockRequest {
-	userId: number;
-	friendId: number;
+	fromUserId: number;
+	toUserId: number;
 	action: 'block' | 'unblock'; // 차단 또는 해제
 }
 
 // 차단/해제 결과 응답
 export interface BlockResult {
+	message: string;
+}
+
+// 사용자 정보 조회
+export type GetUserResponse = BaseApiResponse<User>;
+
+// 사용자 정보 수정
+export type UpdateUserResponse = BaseSuccessResponse<User>;
+
+// 회원 탈퇴
+export type DeleteUserResponse = BaseApiResponse<DeleteUserResult>;
+
+// 회원 탈퇴 응답 데이터
+export interface DeleteUserResult {
 	message: string;
 }
--- src/apis/User/index.ts
+++ src/apis/User/index.ts
@@ -1,5 +1,14 @@
-import { UpdateUserResponse } from './dto';
+import { UpdateUserResponse, CreateUserBlockRequest, UpdateUserReportRequest } from './dto';
 import { newRequest } from '../core';
+import { EmptySuccessResponse } from '../core/dto';
 
 // 유저 정보 수정 api
 export const patchUpdateUser = (userId: string) => newRequest.patch<UpdateUserResponse>(`/users/${userId}`);
+
+// 유저 차단 api
+export const postBlockUser = (data: CreateUserBlockRequest) =>
+	newRequest.post<EmptySuccessResponse>('/user-block', data);
+
+// 유저 신고 api
+export const postReportUser = (data: UpdateUserReportRequest) =>
+	newRequest.post<EmptySuccessResponse>('/user-report', data);
