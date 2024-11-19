d889891ac3995b0c6b383adab4b9b3145700dfe9
지민재 <jim400934@gmail.com>
2024-11-19 13:37:20 +0900

    Chore: 카카오 api 수정, jwt 파싱할 페이지 추ㅏㄱ

--- src/apis/Auth/index.ts
+++ src/apis/Auth/index.ts
@@ -1,7 +1,9 @@
 import { KakaoLoginResponse, NaverLoginResponse } from './dto';
 import { newRequest } from '../core';
 
-export const getKakaoLoginApi = (code: string) => newRequest.get<KakaoLoginResponse>(`/auth/login/kakao?code=${code}`);
+// 카카오 로그인 api, url은 로그인 성공 후 jwt 파싱 위해 리다이렉트 되는 페이지의 url
+export const getKakaoLoginApi = (url: string) => newRequest.get<KakaoLoginResponse>(`/auth/login/kakao?url=${url}`);
 
+// 네이버 로그인 api
 export const getNaverLoginApi = (code: string) =>
 	newRequest.get<NaverLoginResponse>(`/auth/login/naver?code=${code}&state=STATE_TOKEN`);
---
+++ src/pages/Login/loginComplete.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// 이곳에서 경로의 jwt 읽어서 로컬에 저장하고, auth/me로 jwt만으로도 회원 정보 가져올 수 있는 api 호출해서
+// 회원 정보의 닉네임 유무에 따라 홈 또는 회원가입 화면으로 리다이렉트
+
+const loginComplete: React.FC = () => {
+	return <></>;
+};
+
+export default loginComplete;
