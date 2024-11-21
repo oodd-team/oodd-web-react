4cf25a20fc1d226b2282b515070193d1e341694f
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-11-21 19:42:38 +0900

    Rename: api 인터페이스명 통일

--- src/apis/post-like/dto.ts
+++ src/apis/post-like/dto.ts
@@ -2,12 +2,12 @@ import { BaseSuccessResponse } from '../core/dto';
 import { PaginationMeta } from '../util/dto';
 
 // 좋아요 누르기/취소
-export type TogglePostLikeStatusResponse = BaseSuccessResponse<TogglePostLikeStatus>;
+export type TogglePostLikeStatusResponse = BaseSuccessResponse<TogglePostLikeStatusData>;
 
 // 게시물 좋아요 리스트 조회
-export type GetPostLikeListResponse = BaseSuccessResponse<PostLikeListData>;
+export type GetPostLikeListResponse = BaseSuccessResponse<GetPostLikeListData>;
 
-export interface PostLikeListData {
+export interface GetPostLikeListData {
 	likeCount: number;
 	likes: Like[];
 	meta: PaginationMeta;
@@ -24,7 +24,7 @@ export interface Like {
 	createdAt: string;
 }
 
-export interface TogglePostLikeStatus {
+export interface TogglePostLikeStatusData {
 	post: {
 		id: number;
 		isPostLike: boolean;
