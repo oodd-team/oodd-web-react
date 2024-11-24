edeefbb0baa91237b189f769de7860274088cd63
지민재 <jim400934@gmail.com>
2024-11-24 22:19:31 +0900

    Chore: px -> rem 단위 변경

--- src/pages/TermsAgreement/styles.tsx
+++ src/pages/TermsAgreement/styles.tsx
@@ -4,33 +4,33 @@ import { StyledText } from '../../components/Text/StyledText';
 export const TermsAgreementContainer = styled.main`
 	display: flex;
 	flex-direction: column;
-	padding: 30px;
-	gap: 20px;
+	padding: 1.875rem;
+	gap: 1.25rem;
 `;
 export const BackButton = styled.img`
-	width: 30px;
-	height: 30px;
+	width: 1.875rem;
+	height: 1.875rem;
 	cursor: pointer;
 `;
 
 export const CheckboxWrapper = styled.div`
-	margin-top: 20px;
+	margin-top: 1.25rem;
 `;
 
 export const CheckboxItem = styled.div`
 	display: flex;
 	align-items: center;
-	margin-bottom: 10px;
+	margin-bottom: 0.625rem;
 `;
 
 export const CheckboxInput = styled.input`
-	margin-right: 10px;
+	margin-right: 0.625rem;
 	cursor: pointer;
 	appearance: none; /* 기본 스타일 제거 */
-	width: 20px;
-	height: 20px;
-	border: 2px solid #e0e0e0;
-	border-radius: 4px;
+	width: 1.25rem;
+	height: 1.25rem;
+	border: 0.125rem solid #e0e0e0;
+	border-radius: 0.25rem;
 	position: relative;
 	&:checked {
 		background-color: #ffbbda;
@@ -39,7 +39,7 @@ export const CheckboxInput = styled.input`
 	&:checked::after {
 		content: '✓';
 		color: white;
-		font-size: 14px;
+		font-size: 0.875rem;
 		position: absolute;
 		top: 50%;
 		left: 50%;
@@ -49,8 +49,8 @@ export const CheckboxInput = styled.input`
 
 export const Divider = styled.hr`
 	border: none;
-	border-top: 1px solid #e0e0e0;
-	margin: 10px 0;
+	border-top: 0.0625rem solid #e0e0e0;
+	margin: 0.625rem 0;
 `;
 
 export const StyledTitle = styled(StyledText)`
