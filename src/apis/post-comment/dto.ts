7593d6f2d99d806ed870b64b290519116cafa53e
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-11-26 02:23:33 +0900

    Fix: post-comment 도메인 api 모듈 수정

--- src/apis/post-comment/dto.ts
+++ src/apis/post-comment/dto.ts
@@ -1,42 +1,41 @@
-import { BaseSuccessResponse } from "../core/dto";
+import { BaseSuccessResponse } from '../core/dto';
 
-// 공통 필드 인터페이스
-interface BaseComment {
+// 댓글 작성
+// Request
+export type CreateCommentRequest = Content;
+// Response
+export type CreateCommentResponse = BaseSuccessResponse<CreateCommentData>;
+
+//댓글 리스트 조회
+// Response
+export type GetCommentListResponse = BaseSuccessResponse<GetCommentListData>;
+
+// 댓글 삭제
+// Response : empty
+
+interface Content {
+	content: string;
+}
+
+interface CreateCommentData extends Content {}
+
+interface Comment {
 	id: number;
-	userId: number;
-	postId: number;
+	user?: CommentUser;
 	content: string;
 	createdAt: string;
-	updatedAt: string;
+	isCommentWriter: boolean;
 }
 
 // 댓글 관련 User 정보
 export interface CommentUser {
-	id: number;
+	userId: number;
 	nickname: string;
 	profilePictureUrl: string;
 }
 
-// 댓글 인터페이스
-export interface Comment extends BaseComment {
-	status: 'activated' | 'deactivated';
-	deletedAt?: string | null;
-	user?: CommentUser;
-}
-
-// 댓글 작성 응답
-export interface postCommenttResponse extends BaseComment {}
-
-// 댓글 삭제 응답
-export type deleteCommentResponse = 
-BaseSuccessResponse<deleteCommentData>
-
-export interface deleteCommentData extends BaseComment {
-	status: 'deactivated';
-	deletedAt: string;
-}
-
 // 게시글 댓글 리스트 조회 응답
-export interface getCommentResponse {
+export interface GetCommentListData {
 	comments: Comment[];
+	totalComments: number;
 }
