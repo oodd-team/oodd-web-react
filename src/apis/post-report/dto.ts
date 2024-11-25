2492b7496bd429d3c1ab5d25f332ceb54ec4386c
lalaurel <ajinpark0518@naver.com>
2024-11-26 04:12:32 +0900

    fix: 신고하기 api 모듈화 index파일로 분리

--- src/apis/post-report/dto.ts
+++ src/apis/post-report/dto.ts
@@ -1,30 +1,28 @@
-import { newRequest } from "../core";
 import { BaseSuccessResponse } from '../core/dto';
-
-// 게시글 신고 요청 
-export interface SendPostReportRequest {
-  requesterId: number; // 요청자 ID
-  postId: number;      // 게시글 ID
-  reason: string;      // 신고 사유
-}
 
 // 공통 필드 인터페이스
 interface BaseReport {
-	id: number;
-	userId: number;
-	postId: number;
-	content: string;
-	repostReason: string;
+  id: number;           // 신고 ID
+  userId: number;       // 신고 생성 사용자 ID
+  postId: number;       // 신고된 게시글 ID
+  content: string;      // 신고된 게시글 내용
+  repostReason: string; // 신고 사유
 }
 
-// 게시물 신고 응답
-export type SendPostReportResponse = BaseSuccessResponse<SendPostReportData>;
+// 게시글 신고 요청 DTO
+export interface SendPostReportRequest {
+  requesterId: number; // 신고하는 사용자 ID
+  postId: number;      // 신고 대상 게시글 ID
+  reason: string;      // 신고 사유
+}
 
+// 게시물 신고 응답 데이터
 export interface SendPostReportData extends BaseReport {
   requesterId: number; // 신고하는 사용자 ID
-  postId: number; // 신고 대상 게시글 ID
-  reason: string; // 신고 사유 
+  postId: number;      // 신고 대상 게시글 ID
+  reason: string;      // 신고 사유
 }
 
-export const createMatchingApi = (data: SendPostReportRequest) =>
-	newRequest.post<SendPostReportResponse>('/post-report', data);
+// 게시물 신고 응답 타입
+export type SendPostReportResponse = BaseSuccessResponse<SendPostReportData>;
+
---
+++ src/apis/post-report/index.ts
@@ -0,0 +1,7 @@
+
+import { newRequest } from "../core";
+import { SendPostReportRequest, SendPostReportResponse } from "./dto";
+
+// 게시글 신고 API
+export const createPostReportApi = (data: SendPostReportRequest) =>
+  newRequest.post<SendPostReportResponse>('/post-report', data);
