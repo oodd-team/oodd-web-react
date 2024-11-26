ad3016265e3c06090309e5dc4f445ce03bada5c2
lalaurel <ajinpark0518@naver.com>
2024-11-26 13:19:19 +0900

    refacot: 코드리뷰 반영

--- src/apis/post-report/index.ts
+++ src/apis/post-report/index.ts
@@ -2,5 +2,5 @@ import { newRequest } from "../core";
 import { SendPostReportRequest, SendPostReportResponse } from "./dto";
 
 // 게시글 신고 API
-export const createPostReportApi = (data: SendPostReportRequest) =>
+export const sendPostReportApi = (data: SendPostReportRequest) =>
   newRequest.post<SendPostReportResponse>('/post-report', data);
