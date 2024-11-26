29123f06fc2887f93e94c66c7d7e49e7bffbc43e
xxinzzi <156905845+xxinzzi@users.noreply.github.com>
2024-11-26 16:55:42 +0900

    Fix: post-comment 리스트 조회 api params 형태  수정

--- src/apis/post-comment/index.ts
+++ src/apis/post-comment/index.ts
@@ -7,7 +7,8 @@ export const createCommentApi = (postId: number, data: CreateCommentRequest) =>
 	newRequest.post<CreateCommentResponse>(`/post-comment?postId=${postId}`, data);
 
 //  게시글 댓글 리스트 조회
-export const getCommentListApi = (postId: number) => newRequest.get<GetCommentListResponse>(`/post-comment/${postId}`);
+export const getCommentListApi = (postId: number) =>
+	newRequest.get<GetCommentListResponse>(`/post-comment?postId=${postId}`);
 
 // 게시글 댓글 삭제
 export const deleteCommentApi = (commentId: number) =>
