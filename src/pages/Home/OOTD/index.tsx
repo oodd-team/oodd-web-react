9bbb81b2af7a6d616330fc3f72b2e90041e1d7d6
young <azerosong@gmail.com>
2024-11-30 02:39:32 +0900

    Feat: 피드 스크롤  페이징 구현

--- src/pages/Home/OOTD/index.tsx
+++ src/pages/Home/OOTD/index.tsx
@@ -1,36 +1,77 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { OOTDContainer, FeedContainer, OOTDLoading } from './styles';
+import React, { useState, useEffect, useRef, useLayoutEffect } from 'react';
+import { OOTDContainer, FeedContainer } from './styles';
 import Feed from './Feed';
-import Loading from '../../../components/Loading'; // Loading 컴포넌트
 import { getPostListApi } from '../../../apis/post';
 import { PostSummary } from '../../../apis/post/dto';
+import { debounce } from 'lodash';
+import { handleError } from '../../../apis/util/handleError';
+import { ModalProps } from '../../../components/Modal/dto';
+import Modal from '../../../components/Modal';
 
 const OOTD: React.FC = () => {
 	const [feeds, setFeeds] = useState<PostSummary[]>([]);
-	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [reachedEnd, setReachedEnd] = useState(false);
+	const [modalContent, setModalContent] = useState('알 수 없는 오류입니다.\n관리자에게 문의해 주세요.');
+	const [isStatusModalOpen, setIsStatusModalOpen] = useState(false);
 	const feedPageRef = useRef(1);
+	const scrollPositionRef = useRef(0);
 
 	const getPostList = async () => {
-		const response = await getPostListApi(feedPageRef.current, 20);
+		if (reachedEnd) return;
 
-		if (response.isSuccess) {
-			setFeeds([...feeds, ...response.data.post]);
-			setIsLoading(false);
+		try {
+			const response = await getPostListApi(feedPageRef.current, 20);
+
+			if (response.isSuccess) {
+				if (response.data.post.length === 0) {
+					setReachedEnd(true);
+				} else {
+					setFeeds((prevFeeds) => [...prevFeeds, ...response.data.post]);
+					feedPageRef.current += 1;
+				}
+			}
+		} catch (error) {
+			const errorMessage = handleError(error);
+			setModalContent(errorMessage);
+			setIsStatusModalOpen(true);
 		}
 	};
 
+	// 스크롤 이벤트 핸들러 추가
+	const handleScroll = debounce(() => {
+		if (reachedEnd) return;
+
+		if (window.innerHeight + document.documentElement.scrollTop >= document.body.scrollHeight - window.innerHeight) {
+			scrollPositionRef.current = window.scrollY;
+			console.log(scrollPositionRef.current);
+			getPostList();
+		}
+	}, 50);
+
 	useEffect(() => {
 		getPostList();
-	}, [feedPageRef.current]);
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	useLayoutEffect(() => {
+		window.scrollTo(0, scrollPositionRef.current); // 데이터 로드 후 스크롤 위치 복원
+	}, [feeds]); // feeds가 변경될 때 실행
+
+	const statusModalProps: ModalProps = {
+		content: modalContent,
+		onClose: () => {
+			setIsStatusModalOpen(false);
+		},
+	};
 
 	return (
 		<OOTDContainer>
-			{isLoading && (
-				<OOTDLoading>
-					<Loading />
-				</OOTDLoading>
-			)}
-			<FeedContainer style={{ display: isLoading ? 'none' : 'block' }}>
+			{isStatusModalOpen && <Modal {...statusModalProps} />}
+			<FeedContainer>
 				{feeds.map((feed, index) => (
 					<div key={index}>
 						<Feed key={feed.user.userId} feed={feed} />
