987a0eab820cd4b021d2723a89118688eb2d4f4f
lalaurel <ajinpark0518@naver.com>
2024-12-03 16:22:14 +0900

    fix: 로그인 리다이렉션 수정

--- src/pages/AccountCancel/index.tsx
+++ src/pages/AccountCancel/index.tsx
@@ -30,7 +30,6 @@ const AccountCancel: React.FC = () => {
 			const token = localStorage.getItem('new_jwt_token');
 
 			if (!storedUserId || !token) {
-				alert('로그인이 필요합니다. 로그인 페이지로 이동합니다.');
 				return;
 			}
 
