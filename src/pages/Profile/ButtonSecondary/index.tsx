d1b092c06dde6a0e97a6d227baa41969160fa7b5
lalaurel <ajinpark0518@naver.com>
2024-12-18 03:57:46 +0900

    refactor: 버튼 수정

---
+++ src/pages/Profile/ButtonSecondary/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from './styles';
+import { StyledText } from '../../../components/Text/StyledText';
+import theme from '../../../styles/theme';
+
+const ButtonSecondary: React.FC = () => {
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		navigate('/profile/edit'); 
+	};
+
+	return (
+		<Button onClick={handleClick}>
+			<StyledText $textTheme={{ style: 'button1-regular', lineHeight: 2 }} color={theme.colors.gradient}>
+				프로필 수정
+			</StyledText>
+		</Button>
+	);
+};
+
+export default ButtonSecondary;
