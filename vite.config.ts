76da12b14025b456556811dd7a4481e831834815
young <azerosong@gmail.com>
2024-12-19 02:53:58 +0900

    Fix: 절대경로 사용을 위한 alias 설정

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,17 @@ export default defineConfig({
 	define: {
 		'process.env': {},
 	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@styles': path.resolve(__dirname, 'src/styles'),
+			'@apis': path.resolve(__dirname, 'src/apis'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@config': path.resolve(__dirname, 'src/config'),
+			'@context': path.resolve(__dirname, 'src/context'),
+			'@page': path.resolve(__dirname, 'src/page'),
+			'@recoil': path.resolve(__dirname, 'src/recoil'),
+		},
+	},
 });
