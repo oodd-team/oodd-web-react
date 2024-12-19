28f52d4dfd16541df90ede3ac8dd259059ae9aeb
지민재 <jim400934@gmail.com>
2024-12-19 14:32:34 +0900

    Rename: 폴더 이동

--- src/pages/Login/SocialLoginButton/index.tsx
+++ src/pages/Login/SocialLoginButton/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import theme from '../../../../styles/theme';
+import theme from '@/styles/theme';
 import { SocialLoginContainer, LogoImgWrapper, LogoImage, StyledTextWrapper } from './style';
 
 import { SocialLoginProps } from './SocialLoginProps';
