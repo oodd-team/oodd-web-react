2ba7be790076bba4dc50f89ce19fc3ebc3f1856d
young <azerosong@gmail.com>
2024-12-22 04:05:33 +0900

    Feat: eslint-plugin-import 관련 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,64 @@
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:import/typescript',
+		'plugin:import/recommended',
+	],
 	parser: '@typescript-eslint/parser',
-	parserOptions: { project: ['./tsconfig.json'] },
-	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		project: ['./tsconfig.app.json', './tsconfig.node.json'],
+	},
+	plugins: ['@typescript-eslint', 'eslint-plugin-import'],
+	settings: {
+		'import/resolver': {
+			typescript: {
+				project: ['./tsconfig.app.json', './tsconfig.node.json'],
+			},
+		},
+		'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+	},
+	env: {
+		node: true,
+	},
 	rules: {
 		'@typescript-eslint/strict-boolean-expressions': [
-			2,
+			'error',
 			{
 				allowString: false,
 				allowNumber: false,
 			},
 		],
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'import/order': [
+			'error',
+			{
+				groups: ['builtin', 'external', 'internal', 'index', 'type', 'parent', 'sibling', 'object'],
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
+				'newlines-between': 'never',
+			},
+		],
+		'import/no-unresolved': 'off',
+		'import/export': 'off',
 	},
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'],
+			rules: {
+				'import/order': 'error',
+			},
+		},
+	],
 	ignorePatterns: ['src/**/*.test.ts', 'src/frontend/generated/*'],
 };
