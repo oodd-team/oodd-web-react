ffecf27bf20b3b79f0539999d31e6e075ccf2fda
young <azerosong@gmail.com>
2024-12-27 15:14:19 +0900

    Feat: 모든 svg 컴포넌트에 width, height 프로퍼티 추가

--- src/components/Icons/Message.tsx
+++ src/components/Icons/Message.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import type { IconsProps } from './dto';
 
-const Message: React.FC<IconsProps> = ({ color = '', isFilled = false, width, height }) => {
+const Message: React.FC<IconsProps> = ({ color = '', isFilled = false, width = '14', height = '12' }) => {
 	return (
 		<>
 			{isFilled ? ( // isFilled가 true일 때 원하는 색 사용 (desktopNavBar의 message-fill는 color을 black으로, default는 color을 white로)
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width={width || 14}
-					height={height || 12}
-					viewBox="0 0 14 12"
-					fill="none"
-				>
+				<svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 14 12" fill="none">
 					<path
 						d="M12.4 0.25H1.6C0.8575 0.25 0.25 0.8575 0.25 1.6V13.75L2.95 11.05H12.4C13.1425 11.05 13.75 10.4425 13.75 9.7V1.6C13.75 0.8575 13.1425 0.25 12.4 0.25Z"
 						fill={color}
@@ -21,13 +15,7 @@ const Message: React.FC<IconsProps> = ({ color = '', isFilled = false, width, he
 				</svg>
 			) : (
 				// isFilled가 false일 때 원하는 색 사용 (desktopNavBar의 message는 color을 black으로, default는 color을 #8E8E8E로, default의 message-white는 color을 white로)
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width={width || 14}
-					height={height || 12}
-					viewBox="0 0 14 12"
-					fill="none"
-				>
+				<svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 14 12" fill="none">
 					<path
 						d="M12.4 0.25H1.6C0.8575 0.25 0.25 0.8575 0.25 1.6V13.75L2.95 11.05H12.4C13.1425 11.05 13.75 10.4425 13.75 9.7V1.6C13.75 0.8575 13.1425 0.25 12.4 0.25ZM12.4 9.7H2.95L1.6 11.05V1.6H12.4V9.7Z"
 						fill={color}
--- src/components/Icons/MyPage.tsx
+++ src/components/Icons/MyPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import type { IconsProps } from './dto';
 
-const MyPage: React.FC<IconsProps> = ({ color = '', isFilled = false }) => {
+const MyPage: React.FC<IconsProps> = ({ color = '', isFilled = false, width = '16', height = '16' }) => {
 	return (
 		<>
 			{isFilled ? ( // isFilled가 true일 때 원하는 색 사용 (desktopNavBar의 my-page-fill은 color을 black으로, default는 color을 white로)
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+				<svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 16 16" fill="none">
 					<path
 						d="M8 1.9C9.16 1.9 10.1 2.84 10.1 4C10.1 5.16 9.16 6.1 8 6.1C6.84 6.1 5.9 5.16 5.9 4C5.9 2.84 6.84 1.9 8 1.9ZM8 10.9C10.97 10.9 14.1 12.36 14.1 13V14.1H1.9V13C1.9 12.36 5.03 10.9 8 10.9ZM8 0C5.79 0 4 1.79 4 4C4 6.21 5.79 8 8 8C10.21 8 12 6.21 12 4C12 1.79 10.21 0 8 0ZM8 9C5.33 9 0 10.34 0 13V16H16V13C16 10.34 10.67 9 8 9Z"
 						fill={color}
@@ -20,7 +20,7 @@ const MyPage: React.FC<IconsProps> = ({ color = '', isFilled = false }) => {
 				</svg>
 			) : (
 				// isFilled가 false일 때 원하는 색 사용 (desktopNavBar의 my-page는 color을 black으로, default는 color을 #8E8E8E로, default의 my-page-white는 color을 white로)
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+				<svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 16 16" fill="none">
 					<path
 						d="M8 1.9C9.16 1.9 10.1 2.84 10.1 4C10.1 5.16 9.16 6.1 8 6.1C6.84 6.1 5.9 5.16 5.9 4C5.9 2.84 6.84 1.9 8 1.9ZM8 10.9C10.97 10.9 14.1 12.36 14.1 13V14.1H1.9V13C1.9 12.36 5.03 10.9 8 10.9ZM8 0C5.79 0 4 1.79 4 4C4 6.21 5.79 8 8 8C10.21 8 12 6.21 12 4C12 1.79 10.21 0 8 0ZM8 9C5.33 9 0 10.34 0 13V16H16V13C16 10.34 10.67 9 8 9Z"
 						fill={color}
