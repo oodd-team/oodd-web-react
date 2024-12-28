15c5e6c78c386b5b64c37541466c599143ab4225
lalaurel <ajinpark0518@naver.com>
2024-12-29 04:13:52 +0900

    fix: 코리 반영

--- src/components/BottomButton/styles.tsx
+++ src/components/BottomButton/styles.tsx
@@ -8,14 +8,14 @@ export const ButtonWrapper = styled.div`
 	transform: translateX(-50%);
 	width: 100%;
 	height: 6.25rem;
-	background-color: ${({ theme }) => theme.colors.white};
+	background-color: ${({ theme }) => theme.colors.background.primary};
 	justify-content: flex-end;
 	z-index: 1;
 	border: none;
 `;
 
 export const Button = styled.button<{ disabled: boolean }>`
-	background: ${({ disabled, theme }) =>
+	background: ${({ disabled, theme }) => //theme에 없어서 그냥 뒀음
 		disabled ? 'linear-gradient(93deg, #FFC1D6 1.22%, #F8D4D4 99.73%)' : theme.colors.brand.gradient};
 	border-radius: 0.625rem;
 	font-size: 1rem;
