e503eaf3c998616d2f147c3b117fd9c699cb0dd6
Ajin Park <108174693+lalaurrel@users.noreply.github.com>
2025-03-22 15:03:12 +0900

    Feat: 스켈레톤 ui 컴포넌트화

---
+++ src/components/Skeleton/index.tsx
@@ -0,0 +1,39 @@
+import { SkeletonContainer } from './styles';
+
+interface SkeletonProps {
+	width?: string | number;
+	height?: string | number;
+	borderRadius?: string | number;
+	className?: string;
+	style?: React.CSSProperties;
+}
+
+const Skeleton: React.FC<SkeletonProps> = ({
+	width = '100%',
+	height = '16px',
+	borderRadius = '4px',
+	className = '',
+	style = {},
+}) => {
+	// width와 height가 숫자인 경우 px 단위를 추가
+	const getSize = (size: string | number) => {
+		if (typeof size === 'number') {
+			return `${size}px`;
+		}
+		return size;
+	};
+
+	return (
+		<SkeletonContainer
+			className={className}
+			style={{
+				width: getSize(width),
+				height: getSize(height),
+				borderRadius: getSize(borderRadius),
+				...style,
+			}}
+		/>
+	);
+};
+
+export default Skeleton;
